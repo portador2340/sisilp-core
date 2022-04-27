1a30fd051108ffbec0e845f28532989f74c256c4
kllisman <89989898+kllisman@users.noreply.github.com>
2022-04-27 16:59:31 -0500

    "se añadio la clase escuela"

--- src/main/java/edu/ilp/sisgailp/entity/Estudiante.java
+++ src/main/java/edu/ilp/sisgailp/entity/Estudiante.java
@@ -13,40 +13,13 @@ public class Estudiante extends Persona{
     @Column(name="serie",length = 20)
     private String serie;
 
-    @ManyToOne
-    @Column(name="idpersona")
-    private Escuela escuela;
+   // @ManyToOne
+    //@JoinColumn(name="idpersona")
+    //private Escuela escuela;
 
     public Estudiante() {
     }
 
-    public Estudiante(String codigo, String serie, Escuela escuela) {
-        this.codigo = codigo;
-        this.serie = serie;
-        this.escuela = escuela;
-    }
-
-    public Estudiante(Long idpersona, String codigo, String serie, Escuela escuela) {
-        super(idpersona);
-        this.codigo = codigo;
-        this.serie = serie;
-        this.escuela = escuela;
-    }
-
-    public Estudiante(String nombre, String apellidos, int edad, String dni, Date fechaNacimiento, String genero, String codigo, String serie, Escuela escuela) {
-        super(nombre, apellidos, edad, dni, fechaNacimiento, genero);
-        this.codigo = codigo;
-        this.serie = serie;
-        this.escuela = escuela;
-    }
-
-    public Escuela getEscuela() {
-        return escuela;
-    }
-
-    public void setEscuela(Escuela escuela) {
-        this.escuela = escuela;
-    }
 
     public String getCodigo() {
         return codigo;
